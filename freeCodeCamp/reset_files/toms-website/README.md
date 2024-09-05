78bcc450cf2134d9f87258d6294e48a6b87407d9
zandoteach <114046062+zandoteach@users.noreply.github.com>
2024-09-05 11:48:00 -0400

    Delete freeCodeCamp directory

--- freeCodeCamp/reset_files/toms-website/README.md
+++
@@ -1,3 +0,0 @@
-I made this boilerplate
-from the command line
-for the freeCodeCamp bash lessons
--- freeCodeCamp/setup.sh
+++
@@ -1,3 +0,0 @@
-sudo cp /workspace/project/freeCodeCamp/.bashrc ~/.bashrc
-sudo touch /workspace/.bash_history
-sudo chmod 777 /workspace/.bash_history
